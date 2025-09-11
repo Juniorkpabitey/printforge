74c829f38990ca90514d4d1b672956bc9ee4a16e
Juniorkpabitey <juniorkpabitey123@gmail.com>
2025-09-11 16:17:37 +0000

    next/link

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,14 +1,16 @@
 import Image from "next/image";
-//import Link from "next/link";
+import PrintForge from '@/public/printforge_logo.svg'
+import Link from "next/link"
 
 export default function Navbar(){
     return(
         <header className="w-full bg-white">
             <nav className="flex justify-between px-6 py-4">
+                <Link href="/">
                 <div className="relative">
                     {/*Desktop logo */}
                     <Image
-                        src="/printforge_logo.svg"
+                        src={PrintForge.src}
                         alt="printforge logo"
                         width={200}
                         height={40}
@@ -17,17 +19,18 @@ export default function Navbar(){
                     />
                     {/*Mobile Logo */}
                     <Image
-                        src="/printforge_logo.svg"
+                        src={PrintForge.src}
                         alt="printforge logo"
-                        width={40}
-                        height={40}
+                        width={60}
+                        height={60}
                         className="block md:hidden w-[40px] h-auto"
                         priority
                     />
                 </div>
+                </Link>
                 <ul className="flex items-center gap-4">
-                    <a href="">3D Model</a>
-                    <a href="/about">About</a>
+                    <Link href="/3d-models">3D Model</Link>
+                    <Link href="/about">About</Link>
                 </ul>
             </nav>
 
